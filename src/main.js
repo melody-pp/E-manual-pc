e5917830bcc4c15e7269b8cd75258e9f0f42d43e
wangyue <hust_wangyue@foxmail.com>
2018-06-26 09:19:46 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import store from './store'
 
 if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-  location.herf = 'http://www.pview-ad.com/e-manual'
+  location = '/e-manual'
 }
 
 Vue.config.productionTip = false
